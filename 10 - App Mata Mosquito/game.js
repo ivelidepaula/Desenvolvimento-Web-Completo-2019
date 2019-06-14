41210d665173b581de69c78986ffd52775bd64d1
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-06-14 02:21:02 +0200

    Create function for image random positions

--- 10 - App Mata Mosquito/game.js
+++ 10 - App Mata Mosquito/game.js
@@ -5,8 +5,24 @@ function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random()*largura)-90
+    var posicaoY = Math.floor(Math.random()*altura)-90
+
+    console.log(posicaoX, posicaoY)
+
+    //criar elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = 'images/mosca.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
